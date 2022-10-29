cb37132eba9bcf332d42ae255f091450c480c106
Ashley <ashley_nascimento@hotmail.com>
2022-10-29 00:00:04 -0300

    Adcionado imagens das tecnologias e novos componentes

--- src/styles/about.ts
+++ src/styles/about.ts
@@ -1,16 +1,24 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.section`
-    height: 100vh;
-    color: var(--gray-200);
+  
+    color: var(--gray-300);
     position: relative;
     padding: 1rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    b{
+        color: var(--pink);       
+    }
 
     strong{
         display: flex;
         flex-direction: column;
         align-items: center;
-        margin: 2rem;
+        margin: 4rem;
         gap: 1rem;
         font-size: 1.25rem;
         text-transform: uppercase;
@@ -18,6 +26,28 @@ export const Wrapper = styled.section`
         svg{
 
             color: var(--pink);       
+        }
+    }
+
+    & > article{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        max-width: 1200px;
+        width: 100%;   
+        
+        background-color: var(--gray-800);
+        padding: 1rem;
+        border-radius: 8px;
+
+        p{
+            max-width: 500px; 
+         
+        }
+
+        img{
+            width: 400px;
+            height: 400px;
         }
     }
 `
